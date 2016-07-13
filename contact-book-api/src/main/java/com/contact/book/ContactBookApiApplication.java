ca5e2ccaa7b088b2d2cb361162d889d6eaab2448
Marcos Lisboa <marcos.lisboa@hotmart.com.br>
2016-07-12 23:08:00 -0300

    String Spring Bookt Project

---
+++ contact-book-api/src/main/java/com/contact/book/ContactBookApiApplication.java
@@ -0,0 +1,12 @@
+package com.contact.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApiApplication.class, args);
+	}
+}
---
+++ contact-book-api/src/test/java/com/contact/book/ContactBookApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.contact.book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ContactBookApiApplication.class)
+public class ContactBookApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
